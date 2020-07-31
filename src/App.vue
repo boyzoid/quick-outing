ef4aaa1ecf172dad3893ca1a99720c02592397ba
Scott Stroz <scott.stroz@gmail.com>
2020-07-31 15:45:12 -0400

    First Cut

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,32 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app id="inspire">
+    <v-app-bar app color="indigo" dark >
+      <v-toolbar-title>Golf Outing</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <v-container fluid>
+        <v-row align="start" justify="center">
+          <v-col>
+          <home></home>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+    <v-footer color="indigo" app>
+      <span class="white--text">&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import Home from "@/views/Home";
+export default {
+  components: {Home},
+  props: {
+    source: String,
+  },
+  data: () => ({
+  }),
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+</script>
---
+++ src/components/GolfOuting.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-form>
+  <v-container>
+    <v-row class="text-center">
+      <v-col class="mb-0">
+        <p class="display-2 font-weight-bold mb-0">
+          Golf Outing Info
+        </p>
+      </v-col>
+    </v-row>
+    <v-row align="center">
+        <v-text-field class="mr-15" v-model="golfer.name" label="Golfer" required ></v-text-field>
+        <v-text-field class="mr-15" v-model="golfer.score" label="Score" required ></v-text-field>
+        <v-btn color="primary" v-on:click="addGolfer()">Add Golfer</v-btn>
+    </v-row>
+    <v-row v-for="(golfer, index) in golfers" v-bind:key="index">
+      <v-col>{{golfer.name}}</v-col>
+      <v-col>{{golfer.score}}</v-col>
+      <v-btn color="error"><v-icon>fa-trash</v-icon></v-btn>
+    </v-row>
+  </v-container>
+  </v-form>
+</template>
+
+<script>
+  export default {
+    name: 'GolfOuting',
+
+    data: () => ({
+      golfers:[],
+      golfer: {}
+    }),
+    methods:{
+      addGolfer: function(){
+        this.golfers.push( { name: this.golfer.name, score: this.golfer.score } );
+        this.golfer = {}
+      }
+    }
+  }
+</script>
